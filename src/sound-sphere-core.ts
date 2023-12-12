9f222ac9cef5d2be3f9360b6d2947996c6e0e1e0
acgodson <anigodson20@gmail.com>
2023-12-12 05:47:47 +0100

    current graph

--- src/sound-sphere-core.ts
+++ src/sound-sphere-core.ts
@@ -5,8 +5,8 @@ import {
   NewMusicBloc as NewMusicBlocEvent,
   NewSeedStarted as NewSeedStartedEvent,
   SeedCompleted as SeedCompletedEvent,
-  StatusPosted as StatusPostedEvent
-} from "../generated/SoundSphereCore/SoundSphereCore"
+  StatusPosted as StatusPostedEvent,
+} from "../generated/SoundSphereCore/SoundSphereCore";
 import {
   CreatingMusicBloc,
   JoinedMusicBloc,
@@ -14,106 +14,106 @@ import {
   NewMusicBloc,
   NewSeedStarted,
   SeedCompleted,
-  StatusPosted
-} from "../generated/schema"
+  StatusPosted,
+} from "../generated/schema";
 
 export function handleCreatingMusicBloc(event: CreatingMusicBlocEvent): void {
   let entity = new CreatingMusicBloc(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.creator = event.params.creator
+  );
+  entity.bloc = event.params.bloc;
+  entity.creator = event.params.creator;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleJoinedMusicBloc(event: JoinedMusicBlocEvent): void {
   let entity = new JoinedMusicBloc(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.seedBox = event.params.seedBox
-  entity.creator = event.params.creator
+  );
+  entity.bloc = event.params.bloc;
+  entity.seedBox = event.params.seedBox;
+  entity.creator = event.params.creator;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMerged(event: MergedEvent): void {
   let entity = new Merged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.seedId = event.params.seedId
+  );
+  entity.bloc = event.params.bloc;
+  entity.seedId = event.params.seedId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleNewMusicBloc(event: NewMusicBlocEvent): void {
   let entity = new NewMusicBloc(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.creator = event.params.creator
+  );
+  entity.bloc = event.params.bloc;
+  entity.creator = event.params.creator;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleNewSeedStarted(event: NewSeedStartedEvent): void {
   let entity = new NewSeedStarted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.seed = event.params.seed
-  entity.round = event.params.round
+  );
+  entity.bloc = event.params.bloc;
+  entity.seed = event.params.seed;
+  entity.round = event.params.round;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSeedCompleted(event: SeedCompletedEvent): void {
   let entity = new SeedCompleted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.musicBloc = event.params.musicBloc
-  entity.seedId = event.params.seedId
+  );
+  entity.musicBloc = event.params.musicBloc;
+  entity.seedId = event.params.seedId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleStatusPosted(event: StatusPostedEvent): void {
   let entity = new StatusPosted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bloc = event.params.bloc
-  entity.message = event.params.message
-  entity.author = event.params.author
+  );
+  entity.bloc = event.params.bloc;
+  entity.message = event.params.message.toString();
+  entity.author = event.params.author;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
